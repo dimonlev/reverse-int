b04f9576c4b1b382d7697fc7402b81cf55d3b286
dimonlev <dimon_lev@mail.ru>
2020-10-24 18:33:16 +0300

    init: start reverse-int-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let newString = "";
+  let str = n.toString();
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString = newString + str[i];
+  }
+  return parseInt(newString, 10);
+
 }
